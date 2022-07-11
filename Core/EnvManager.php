2b25c906d0d00357f2218159fdcb7377ba21eaf2
programmer test <programmer@sabahost.localsrv>
2022-07-11 10:23:41 +0430

    parsing improvements

--- Core/EnvManager.php
+++ Core/EnvManager.php
@@ -4,21 +4,22 @@ namespace Framework\Core;
 
 class EnvManager
 {
-    public static function initialize(string $envFilePath) : bool
-    {
-        if (!file_exists($envFilePath)) {
-            throw new \Exception("ENV file not found!");
-        }
-        define('ENV', file_get_contents($envFilePath));
-        define("ENTRIES", explode(PHP_EOL, ENV));
-        foreach (ENTRIES as $entry) {
-            $entry = explode('=', $entry);
-            if (!str_contains($entry[1], "\"") && intval($entry[1]) == $entry[1]) {
-                $_ENV[$entry[0]] = intval($entry[1]);
-            } else {
-                $_ENV[$entry[0]] = trim($entry[1], "\"");
-            }
-        }
-        return !empty($_ENV);
-    }
+	public static function initialize(string $envFilePath) : bool
+	{
+		if ( !file_exists($envFilePath) || !is_file($envFilePath) )
+			throw new \Exception("ENV file not found!");
+
+		$fileData = file_get_contents($envFilePath);
+		$entries  = preg_split("/[\r\n]+/", $fileData, -1, PREG_SPLIT_NO_EMPTY);
+
+		foreach ($entries as $entry) {
+			$entry = explode('=', $entry, 2);
+			if (ctype_digit($entry[1])) {
+				$_ENV[$entry[0]] = intval($entry[1]);
+			} else {
+				$_ENV[$entry[0]] = trim($entry[1], "\"");
+			}
+		}
+		return !empty($_ENV);
+	}
 }
