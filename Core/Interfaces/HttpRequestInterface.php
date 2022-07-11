89983d13dc9d4499471ecf8d8ed87c318369a8c4
kouroshmoshrefi <kouroshmoshrefi@hotmail.com>
2022-07-11 16:30:06 +0430

    added get and post methods to http interface

--- Core/Interfaces/HttpRequestInterface.php
+++ Core/Interfaces/HttpRequestInterface.php
@@ -4,4 +4,9 @@ namespace Framework\Core\Interfaces;
 
 interface HttpRequestInterface
 {
+    
+    public function get( string $url, array $params, int $timeout, int $maxTryCount ) : array;
+
+    public function post( string $url, array $params, int $timeout, int $maxTryCount  ) : array;
+
 }
