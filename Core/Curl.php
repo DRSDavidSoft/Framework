0cd0816e8ee617c0fbf79e4ddb887b415b9c50b3
David Refoua <David@Refoua.me>
2022-07-11 15:55:56 +0430

    add Curl interface

---
+++ Core/Curl.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Framework\Core;
+
+use Framework\Core\interfaces\CurlInterface;
+
+/**
+ * File: Curl.php
+ * Author: David@Refoua.me
+ * Author: kouroshmoshrefi@hotmail.com
+ * Version: 0.1.0
+ */
+
+class Curl implements CurlInterface
+{
+}
