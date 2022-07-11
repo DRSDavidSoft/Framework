9ceee868bcbe96c508e0a41b7bf6c98db458f0d6
David Refoua <David@Refoua.me>
2022-07-11 16:32:17 +0430

    add constructor, deconstructor, setURL and setOptions methods

--- Core/HttpRequest.php
+++ Core/HttpRequest.php
@@ -4,6 +4,7 @@ namespace Framework\Core;
 
 use Framework\Core\interfaces\HttpRequestInterface;
 use CurlHandle;
+use Exception;
 
 /**
  * File: HttpRequest.php
@@ -19,5 +20,36 @@ class HttpRequest implements HttpRequestInterface
      * @var CurlHandle instance of the Curl request
      */
     private readonly CurlHandle $ch;
+
+    public function __construct(string $url = null)
+    {
+        // Check if all the required extensions are present
+        foreach ( ['curl'] as $extension ) if( !extension_loaded($extension) ) {
+            throw new \Exception("The required '$extension' extension is not enabled.");
+        }
+
+        $this->ch = curl_init($url);
+
+        if (!$this->ch)
+            throw new \Exception("Failed to initialize the Curl resource!");
+    }
+
+    public function __destruct()
+    {
+        curl_close($this->ch);
+    }
+
+    public function setURL(string $url) : bool
+    {
+        if (!filter_var($url, FILTER_VALIDATE_URL))
+            throw new \Exception("The specified URL is not valid!");
+
+        return $this->setOptions([CURLOPT_URL => &$url]);
+    }
+
+    public function setOptions(array $opts)
+    {
+        return curl_setopt_array($this->ch, $opts);
+    }
 
 }
