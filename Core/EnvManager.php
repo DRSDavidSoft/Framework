d6c0145d782113c63138adbd37a5262140dfb279
programmer test <programmer@sabahost.localsrv>
2022-07-11 10:27:35 +0430

    read ENV file line by line

--- Core/EnvManager.php
+++ Core/EnvManager.php
@@ -2,24 +2,34 @@
 
 namespace Framework\Core;
 
+use Exception;
+
 class EnvManager
 {
 	public static function initialize(string $envFilePath) : bool
 	{
-		if ( !file_exists($envFilePath) || !is_file($envFilePath) )
+		if (!file_exists($envFilePath) || !is_file($envFilePath))
 			throw new \Exception("ENV file not found!");
 
-		$fileData = file_get_contents($envFilePath);
-		$entries  = preg_split("/[\r\n]+/", $fileData, -1, PREG_SPLIT_NO_EMPTY);
+		$handle = fopen($envFilePath, 'r');
 
-		foreach ($entries as $entry) {
-			$entry = explode('=', $entry, 2);
+		if (!$handle)
+			throw new \Exception("Unable to open ENV file!");
+
+		while(($line = fgets($handle)) !== false) {
+			$entry = explode('=', $line = trim($line), 2);
 			if (ctype_digit($entry[1])) {
 				$_ENV[$entry[0]] = intval($entry[1]);
 			} else {
 				$_ENV[$entry[0]] = trim($entry[1], "\"");
 			}
 		}
+
+		if (!feof($handle))
+			throw new Exception("Unexpected end of file!");
+
+		fclose($handle);
+
 		return !empty($_ENV);
 	}
 }
